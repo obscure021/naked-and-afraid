5076fc2b4566c0de62384f71caa2b5b5f7bf01ec
obscure021 <ma930213@gmail.com>
2024-07-25 18:45:17 +0500

    Add gamerule check in the client code to prevent crash on servers without this mod

--- src/client/java/com/obscure/nakedandafraid/NakedAndAfraidClient.java
+++ src/client/java/com/obscure/nakedandafraid/NakedAndAfraidClient.java
@@ -2,16 +2,39 @@ package com.obscure.nakedandafraid;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.option.Perspective;
 import net.minecraft.text.Text;
 
 public class NakedAndAfraidClient implements ClientModInitializer {
+	public static boolean hasGameRule = true;
+
 	@Override
 	public void onInitializeClient() {
+		ClientPlayConnectionEvents.INIT.register((handler, client) -> {
+			// check if the server has game rule is present
+			if (client.world == null) {
+				NakedAndAfraid.LOGGER.info("Client World is null");
+				return;
+			}
+
+			try {
+				client.getServer().getGameRules().get(NakedAndAfraid.NAKED_AND_AFRAID).get();
+				NakedAndAfraid.LOGGER.info("Game rule found");
+			} catch (Exception e) {
+				NakedAndAfraid.LOGGER.info("Game rule not found");
+				hasGameRule = false;
+			}
+		});
+
+		if (!hasGameRule) {
+			return;
+		}
+		
 		// Change perspective to first person at the end of each tick
 		ClientTickEvents.END_CLIENT_TICK.register((client) -> {
 			// check if the server has game rule is present
-			if (client.world == null) {
+			if (client.world == null || client.getServer() == null) {
 				return;
 			}
 
